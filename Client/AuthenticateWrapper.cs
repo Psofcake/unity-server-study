2cca31c0311a4013455c83ec9a1750485be370f9
Psofcake <g.psofcake@gmail.com>
2025-08-13 01:18:00 +0900

    08.13 Netcode
    로그인/인증시도 ->  성공 시 메뉴씬 로드하기

--- Client/AuthenticateWrapper.cs
+++ Client/AuthenticateWrapper.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Unity.Services.Authentication;
+using Unity.Services.Core;
+using UnityEngine;
 
 
 public static class AuthenticateWrapper
@@ -12,26 +15,68 @@ public static class AuthenticateWrapper
     {
         if (authState == AuthState.Authenticated) return authState; //이미 인증이 되어있으면 아래 과정 스킵
 
+        if (authState == AuthState.Authenticating)
+        {
+            //인증 중일때 대기
+            Debug.LogWarning("이미 인증 중입니다.");
+            await Authenticating();
+            return authState;
+        }
+        
+        await SignInAnonymousAsync(triedCount);
+        
+        return authState;
+    }
+
+    private static async Task<AuthState> Authenticating()
+    {
+        while(authState == AuthState.Authenticating||authState == AuthState.NotAuthenticated) await Task.Delay(300);
+        return authState;
+    }
+
+    private static async Task SignInAnonymousAsync(int triedCount)
+    {
         authState = AuthState.Authenticating;   //인증 진행중으로 상태 변경.
         int count = 0;
 
         while (authState == AuthState.Authenticating && count < triedCount)
         {
             /* UGS를 통한 인증 과정 */
-            await AuthenticationService.Instance.SignInAnonymouslyAsync();  //Unity의 AuthenticationService를 이용해 익명 로그인을 시도. try-catch추가 필요
-            //await AuthenticationService.Instance.SignInWith
-            if (AuthenticationService.Instance.IsSignedIn && AuthenticationService.Instance.IsAuthorized)   //인증 성공 && UGS에 대해 유효한 액세스 토큰을 가지고 있는지(서버와 통신 가능한 인증상태) = 사용자 세션이 완전히 유효한지 더블체크
+            try
             {
-                //Debug.Log("Auth Success");
-                authState = AuthState.Authenticated;
-                break;
+                await AuthenticationService.Instance
+                    .SignInAnonymouslyAsync(); //Unity의 AuthenticationService를 이용해 익명 로그인을 시도. try-catch 필요
+                //await AuthenticationService.Instance.SignInWith
+
+                if (AuthenticationService.Instance.IsSignedIn &&
+                    AuthenticationService.Instance
+                        .IsAuthorized) //인증 성공 && UGS에 대해 유효한 액세스 토큰을 가지고 있는지(서버와 통신 가능한 인증상태) = 사용자 세션이 완전히 유효한지 더블체크
+                {
+                    //Debug.Log("Auth Success");
+                    authState = AuthState.Authenticated;
+                    break;
+                }
+            }
+            catch (AuthenticationException e)
+            {
+                Debug.LogError(e.Message);
+                authState = AuthState.Failed;
+            }
+            catch (RequestFailedException e)
+            {
+                Debug.LogError(e.Message);
+                authState = AuthState.Failed;
             }
 
             count++;
             await Task.Delay(1000);
         }
-
-        return authState;
+        //모든 시도가 실패
+        if (authState != AuthState.Authenticated)
+        {
+            Debug.LogWarning($"플레이어 인증 실패: {count} 번 시도.");
+            authState = AuthState.Timeout; 
+        }
     }
 }
 
--- Client/ClientGameManager.cs
+++ Client/ClientGameManager.cs
@@ -12,7 +12,12 @@ public class ClientGameManager : MonoBehaviour
     public async Task<bool> InitAsync()
     {
 		await UnityServices.InitializeAsync();	//UGS의 핵심 기능을 사용하기 전에 가장 먼저 이 메서드를 호출하여 초기화 작업을 수행해야 한다.
-
+		/* UnityServices.InitializeAsync가 수행하는 작업:
+		1. Unity 프로젝트와 연결된 환경(environment)을 확인
+		2. 로컬 설정(Unity 프로젝트 설정)과 클라우드 환경을 동기화
+		3. Unity Gaming Services의 모듈들(Authentication, Cloud Save 등)을 초기화
+		4. 기본 사용자 세션 준비	*/
+		
         //플레이어 인증 처리
 		AuthState authState = await AuthenticateWrapper.DoAuth();
 		if (authState == AuthState.Authenticated){
